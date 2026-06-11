a38505c072b2f1837acfee089341673eab15e00d
Jeremy Gael <jeremygael0108@gmail.com>
2026-06-11 15:00:02 -0500

    modificando

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Variables
+        int num1, num2, suma;
+
+        // Entrada de datos
+        Console.Write("Ingrese el primer número: ");
+        num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo número: ");
+        num2 = int.Parse(Console.ReadLine());
+
+        // Proceso
+        suma = num1 + num2;
+
+        // Salida de datos
+        Console.WriteLine("La suma es: " + suma);
+    }
+}
